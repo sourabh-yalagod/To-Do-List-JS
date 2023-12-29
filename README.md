6c2dfa66829756c3903152ddeec50594f1202385
sourabh-yalagod <sourabhofficial99804@gmail.com>
2023-12-29 20:46:42 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# TO DO LIST - Java Script
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+- mobile vesion 
+- adding a task and deleting the completed one 
+
+### Screenshot
+
+![](./Images/screenShot%20(1).png)
+![](./Images/screenShot%20(2).png)
+
+### Links
+
+- Solution URL: [Solution on GitHub](https://github.com/sourabh-yalagod/To-Do-List-JS)
+- Live Site URL: [Live site](https://sourabh-yalagod.github.io/To-Do-List-JS/)
+
+### Built with
+
+- java script
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+
---
+++ script.js
@@ -0,0 +1,56 @@
+
+const tasksContainer = document.getElementById('tasks');
+const newTaskInput = document.getElementById('newTask');
+const errorMsg = document.querySelector('.errorMsg');
+console.log(errorMsg);
+
+function addTask() {
+  const taskText = newTaskInput.value.trim();
+
+  if (taskText === '') {
+    // alert('Please enter a task.');
+    newTaskInput.style.border='1px solid red';
+    errorMsg.innerText='can not be empty';
+    return;
+  }
+  newTaskInput.style.border='1px solid gray';
+  errorMsg.innerText='';
+  
+  const taskItem = document.createElement('div');
+  taskItem.className = 'task-item';
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  
+  const division = document.createElement('p');
+  division.className = 'division';
+
+  const taskTextElement = document.createElement('span');
+  taskTextElement.textContent = taskText;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+
+  deleteButton.onclick = function () {
+    tasksContainer.removeChild(taskItem);
+  };
+  taskItem.appendChild(division);
+
+  division.appendChild(checkbox);
+  division.appendChild(taskTextElement);
+  taskItem.appendChild(deleteButton);
+
+  tasksContainer.appendChild(taskItem);
+
+  newTaskInput.value = '';
+}
+
+// Delete Completed Tasks Function
+function deleteCompletedTasks() {
+  const completedTasks = document.querySelectorAll('.task-item input[type="checkbox"]:checked');
+
+  completedTasks.forEach((task) => {
+    const taskItem = task.parentElement;
+    tasksContainer.removeChild(taskItem);
+  });
+}
